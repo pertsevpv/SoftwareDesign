384ec88092cf333e42da46cb4d85bf58d4648ca5
pertsevpv <pertsevpv@yandex.ru>
2022-10-30 13:52:40 +0300

    HW-3: Clean unused dependencies

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
